4f5e2a199bc836f4e36732e385ea99c3a54fd030
Rian Chandra <rianc@uw.edu>
2026-04-15 17:28:10 -0400

    Added support for older setup.py file

--- setup.py
+++ setup.py
@@ -11,12 +11,10 @@ setup(
     name='eqtools',
     version='1.5.0',
     packages=['eqtools'],
-    install_requires=['scipy', 'numpy', 'matplotlib'],
     author=['Mark Chilenski', 'Ian Faust', 'John Walk'],
     author_email='psfcplasmatools@mit.edu',
     url='https://github.com/PSFCPlasmaTools/eqtools/',
     description='Python tools for magnetic equilibria in tokamak plasmas',
     long_description=open('README.md', 'r').read(),
     ext_modules=[tricub],
-    license='GPL'
 )
